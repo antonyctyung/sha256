78147acf8087d2ed1cb625426a0c4c991a6d729c
Antony Yung <22206795+antonyctyung@users.noreply.github.com>
2021-11-23 22:54:49 -0800

    removed unused test vector

--- src/sha256_core_tb.sv
+++ src/sha256_core_tb.sv
@@ -14,32 +14,6 @@ case (n)
 endcase
 endmodule
 
-module sha256_core_tb_vec_gen_1(
-    input integer n,
-    output logic [31:0] data
-);
-always_comb
-case (n)
-     0: data <= 32'h61626364;
-     1: data <= 32'h62636465;
-     2: data <= 32'h63646566;
-     3: data <= 32'h64656667;
-     4: data <= 32'h65666768;
-     5: data <= 32'h66676869;
-     6: data <= 32'h6768696A;
-     7: data <= 32'h68696A6B;
-     8: data <= 32'h696A6B6C;
-     9: data <= 32'h6A6B6C6D;
-    10: data <= 32'h6B6C6D6E;
-    11: data <= 32'h6C6D6E6F;
-    12: data <= 32'h6D6E6F70;
-    13: data <= 32'h6E6F7071;
-    14: data <= 32'h80000000;
-    15: data <= 32'b01011000;
-    default: data <= 32'h00000000;
-endcase
-endmodule
-
 module sha256_core_tb;
 
     localparam T = 10ns;
